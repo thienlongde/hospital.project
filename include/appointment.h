dac4a661c6774ae3e28c87b8af142a41c5cedef3
Long Thien <longthien14092007@gamil.com>
2026-05-24 17:34:45 +0700

    update

--- include/appointment.h
+++ include/appointment.h
@@ -9,5 +9,5 @@ void searchInFile(const char *fileName, const char *searchKey);
 bool isValidInfo(const char *input);
 void deleteAppointment(const char *fileName, const char *searchKey);
 void processDeleteAction(const char *fileName);
-void getpatientChoice(int patientChoice);
+bool isPatientExist(const char *fileName, const char *searchKey);
 #endif
